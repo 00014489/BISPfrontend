4f3d777ee286b31d8d9e44fc3e8b92191f20f855
Usmonov Elbek <usmonovelbek7@gmail.com>
2025-04-28 09:35:34 +0500

    changing ips for testing

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl: 'http://localhost:4200',
-  apiUrl: 'https://20.174.11.170',
+  apiUrl: 'http://localhost:4200',
+  // apiUrl: 'https://20.174.11.170',
   telegramFileApiUrl: 'https://api.telegram.org/file/bot',
   botToken: '8157882201:AAH9C67VbL47h9gFCRxCYpyvrRHqareWCVE'
   // botToken: '7756963617:AAFAx5qG31in-0BiMOmkrkXIG1kPOP1BXYM' // Replace with your actual bot token
